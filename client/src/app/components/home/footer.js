4e04bbe5df20c02c9c4ff026041d809fb9736ba2
Rudranil <rudranilm2004@gmail.com>
2025-07-23 22:48:06 +0530

    Few Changes and footer added

--- client/src/app/components/home/footer.js
+++ client/src/app/components/home/footer.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import styles from '../../../styles/home/footer/footer.module.css'
+import Link from 'next/link'
 
 const Footer = () => {
   return (
-    <div>
-      Footer
+    <main className={styles.footer}>
+      <div className={styles.img}>
+      <img src='/logo.png' className={styles.logo}/>
+      </div>
+      <div className={styles.list}>
+      <ul className={styles.ul}>
+        <li><Link href="/" >About us</Link></li>
+        <li><Link href="/">Contact Us</Link></li>
+        <li><Link href="/">Policy</Link></li>
+      </ul>
     </div>
+    <p className={styles.para}>Obscura © 2025. All Rights Reserved. Built for film fans, by film fans.</p>
+    </main>
   )
 }
 
